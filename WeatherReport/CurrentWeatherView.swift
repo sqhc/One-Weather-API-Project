d13d4943841695cd4cd239c78fa38181cdf3374a
shenqing <shenqin1@msu.edu>
2023-04-05 20:54:38 -0400

    IBOutlet for the components on the current weather view.

--- WeatherReport/CurrentWeatherView.swift
+++ WeatherReport/CurrentWeatherView.swift
@@ -6,9 +6,29 @@
 //
 
 import UIKit
+import MapKit
 
 class CurrentWeatherView: UIViewController {
-
+    @IBOutlet weak var cityLabel : UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var minimumTempLabel: UILabel!
+    @IBOutlet weak var highestTempLabel: UILabel!
+    @IBOutlet weak var feelsLikeTempLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var windDegreeLabel: UILabel!
+    @IBOutlet weak var windGustLabel: UILabel!
+    @IBOutlet weak var cloudlinessLabel: UILabel!
+    @IBOutlet weak var rainOneHourLabel: UILabel!
+    @IBOutlet weak var rainThreeHourLabel: UILabel!
+    @IBOutlet weak var snowOneHourLabel: UILabel!
+    @IBOutlet weak var snowThreeHourLabel: UILabel!
+    @IBOutlet weak var cityMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
