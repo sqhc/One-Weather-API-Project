4aea92ad52fa1d87bc5de8127acf32197add62c8
shenqing <shenqin1@msu.edu>
2023-04-08 15:21:19 -0400

    The layout for geocode searching page, implement the data model for geocode of searching by city.

---
+++ WeatherReport/Model/GeocodeDataModels.swift
@@ -0,0 +1,16 @@
+//
+//  GeocodeDataModels.swift
+//  WeatherReport
+//
+//  Created by 沈清昊 on 4/8/23.
+//
+
+import Foundation
+
+struct CityGeocode: Codable{
+    let name: String?
+    let lat: Double?
+    let lon: Double?
+    let country: String?
+    let state: String?
+}
