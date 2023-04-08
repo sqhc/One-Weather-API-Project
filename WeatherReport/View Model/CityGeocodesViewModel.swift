a3ff8f5bbb14181ac75ac1f6f80e88030c107f80
shenqing <shenqin1@msu.edu>
2023-04-08 16:51:51 -0400

    Create the layout for displaying city geocodes, files for this view and view models.

---
+++ WeatherReport/View Model/CityGeocodesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CityGeocodesViewModel.swift
+//  WeatherReport
+//
+//  Created by 沈清昊 on 4/8/23.
+//
+
+import Foundation
---
+++ WeatherReport/View/CityGeocodeCell.swift
@@ -0,0 +1,23 @@
+//
+//  CityGeocodeCell.swift
+//  WeatherReport
+//
+//  Created by 沈清昊 on 4/8/23.
+//
+
+import UIKit
+
+class CityGeocodeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
